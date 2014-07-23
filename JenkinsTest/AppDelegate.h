6af1f5fac75d34b15f54367233f2fcdf6b174475
Ashwarya Singh <ashwarya.singh@Mac133.local>
2014-07-23 13:22:32 +0530

    Initial Commit

---
+++ JenkinsTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JenkinsTest
+//
+//  Created by Ashwarya Singh on 7/23/14.
+//  Copyright (c) 2014 A1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JenkinsTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  JenkinsTest
+//
+//  Created by Ashwarya Singh on 7/23/14.
+//  Copyright (c) 2014 A1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
